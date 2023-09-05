bf30b6a1a8cdfc979bb47d0acf54ba4d8aee339b
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2023-09-05 08:03:28 +0200

    SO object

---
+++ ChatterDrive/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI uiText;  // Reference to the UI Text component
+    public List<DialogComponent> Dialogs;  // List of DialogComponent objects
+    public DialogComponent CurrentDialogComponent;
+    private int currentEntryIndex = 0;  // To keep track of the dialog progress
+
+    public void SetCurrentDialog(int index)
+    {
+        if (index >= 0 && index < Dialogs.Count)
+        {
+            CurrentDialogComponent = Dialogs[index];
+            currentEntryIndex = 0;  // Reset the dialog index
+        }
+    }
+
+    public void DisplayDialog()
+    {
+        string dialogText = CurrentDialogComponent.GetDialogText();
+        uiText.text = dialogText;
+        CurrentDialogComponent.Display();
+    }
+
+    public void AdvanceDialog()
+    {
+        if (CurrentDialogComponent is Dialog)
+        {
+            Dialog dialog = (Dialog)CurrentDialogComponent;
+            if (currentEntryIndex < dialog.Components.Count)
+            {
+                uiText.text = dialog.Components[currentEntryIndex].GetDialogText();
+                currentEntryIndex++;
+            }
+        }
+    }
+}
+
+
+
+
+
