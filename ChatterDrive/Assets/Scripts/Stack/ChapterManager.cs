0f3e6bdf710d9bd176d7216590c856e69fd7757b
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2023-10-26 09:28:44 +0200

    Build and namespace error correction

--- ChatterDrive/Assets/Scripts/Stack/ChapterManager.cs
+++ ChatterDrive/Assets/Scripts/Stack/ChapterManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class ChapterManager : MonoBehaviour
